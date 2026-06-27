fc4ccc3293dd12e1d6be4644854e539386b247f4
Carolina <carito940905@gmail.com>
2026-06-27 17:05:15 -0500

    Desembocadura exercise

---
+++ challengecodificadas/Desembocadura.java
@@ -0,0 +1,12 @@
+package challengecodificadas;
+
+public class Desembocadura {
+     public static void main(String[] args) {
+        System.out.println("6 1");
+        System.out.println("1 3");
+        System.out.println("1 4");
+        System.out.println("1 2");
+        System.out.println("2 5");
+        System.out.println("2 6");
+    }
+}
